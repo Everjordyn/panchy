4b011149b4be0fa2af150e543233ea073c669dca
ever panchy <chinacho07@gmail.com>
2022-09-01 14:28:01 -0500

    programa suma

--- suma.cpp
+++ suma.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 using namespace std;
-int main ()
+int main()
 {
-	cout<<endl<<endl;
-	cout<<"hola mundo soy Jorfyn";
-	cout<<endl<<endl;
-	return(0);
+	float x,y,z;
+	cout<<"ingrese 2 numero separados de espacio x y:";
+	cin>>x>>y;
+	z=x+y;
+	cout<<"El resultado es:";
+	cout<<z<<endl;
+	cout<<z;
+	return 0;
 }
-
